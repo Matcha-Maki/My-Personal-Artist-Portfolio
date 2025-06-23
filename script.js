58743a3f073b4ce4aa15c14be6823bacb0a6c7ae
Matcha-Maki <120488088+Matcha-Maki@users.noreply.github.com>
2025-06-23 12:14:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle sidebar menu
+const hamburger = document.getElementById('hamburger');
+const sidebar = document.getElementById('sidebar');
+
+hamburger.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+});
